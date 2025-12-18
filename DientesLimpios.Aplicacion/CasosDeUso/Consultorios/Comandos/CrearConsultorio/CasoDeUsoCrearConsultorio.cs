b7cdbd988f659a586e302ebd3622c4df842aa9dc
idealiveConsulting <idealiveConsulting@gmail.com>
2025-12-18 20:19:23 +0100

    Añade capa Aplicacion y caso de uso CrearConsultorio
    Se agrega el proyecto DientesLimpios.Aplicacion con configuración para .NET 9.0 y referencia a Dominio. Incluye contratos de repositorio genérico y específico para consultorios, así como el caso de uso y comando para crear consultorios. Se actualiza la solución para incluir el nuevo proyecto y dependencias.

---
+++ DientesLimpios.Aplicacion/CasosDeUso/Consultorios/Comandos/CrearConsultorio/CasoDeUsoCrearConsultorio.cs
@@ -0,0 +1,26 @@
+﻿using DientesLimpios.Aplicacion.Contratos.Repositorios;
+using DientesLimpios.Dominio.Entidades;
+
+namespace DientesLimpios.Aplicacion.CasosDeUso.Consultorios.Comandos.CrearConsultorio;
+
+public class CasoDeUsoCrearConsultorio
+{
+    private readonly IRepositorioConsultorios _repositorio;
+    public CasoDeUsoCrearConsultorio(IRepositorioConsultorios repositorio)
+    {
+        _repositorio = repositorio;
+    }
+
+
+
+    public async Task<Guid> Handle(ComandoCrearConsultorio comando)
+    {
+        // Lógica para crear un consultorio - Orquestación del dominio
+        // Por ejemplo, validar datos, guardar en base de datos, etc.
+        // Simulación de creación y retorno de un ID único para el consultorio creado
+        var consultorio = new Consultorio(comando.Nombre);
+        var respuesta = await _repositorio.Agregar(consultorio);
+        return respuesta.Id;
+
+    }
+}
