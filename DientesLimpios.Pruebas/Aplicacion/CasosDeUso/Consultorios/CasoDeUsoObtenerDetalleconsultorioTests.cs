8b9a0c23ba35c0dfdb48c05ad43ddd897cc6f660
idealiveConsulting <idealiveConsulting@gmail.com>
2026-03-25 20:06:21 +0100

    Añadir clase de prueba vacía para detalle de consultorio
    Se ha creado el archivo CasoDeUsoObtenerDetalleconsultorioTests.cs con una clase interna vacía destinada a futuras pruebas del caso de uso de obtención de detalle de consultorio. Incluye las directivas using necesarias, pero aún no contiene métodos de prueba implementados.

---
+++ DientesLimpios.Pruebas/Aplicacion/CasosDeUso/Consultorios/CasoDeUsoObtenerDetalleconsultorioTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DientesLimpios.Pruebas.Aplicacion.CasosDeUso.Consultorios
+{
+    internal class CasoDeUsoObtenerDetalleconsultorioTests
+    {
+    }
+}
