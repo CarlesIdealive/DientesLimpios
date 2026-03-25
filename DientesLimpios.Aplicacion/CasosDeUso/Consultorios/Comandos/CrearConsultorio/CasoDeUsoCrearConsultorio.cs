148eccfa2b5b5ce8a7802331eedc40a1badaf9b1
idealiveConsulting <idealiveConsulting@gmail.com>
2026-03-25 10:36:03 +0100

    Elimina FluentValidation de CasoDeUsoCrearConsultorio
    Se eliminan las referencias y el uso de FluentValidation en la clase CasoDeUsoCrearConsultorio, incluyendo la inyección del validador, la validación del comando y el manejo de la excepción de validación. Ahora el comando se procesa sin validación previa en este caso de uso.

--- DientesLimpios.Aplicacion/CasosDeUso/Consultorios/Comandos/CrearConsultorio/CasoDeUsoCrearConsultorio.cs
+++ DientesLimpios.Aplicacion/CasosDeUso/Consultorios/Comandos/CrearConsultorio/CasoDeUsoCrearConsultorio.cs
@@ -1,9 +1,7 @@
 ﻿using DientesLimpios.Aplicacion.Contratos.Persistencia;
 using DientesLimpios.Aplicacion.Contratos.Repositorios;
-using DientesLimpios.Aplicacion.Excepciones;
 using DientesLimpios.Aplicacion.Utilidades.Mediador;
 using DientesLimpios.Dominio.Entidades;
-using FluentValidation;
 
 namespace DientesLimpios.Aplicacion.CasosDeUso.Consultorios.Comandos.CrearConsultorio;
 
@@ -11,17 +9,14 @@ public class CasoDeUsoCrearConsultorio : IRequestHandler<ComandoCrearConsultorio
 {
     private readonly IRepositorioConsultorios _repositorio;
     private readonly IUnidadDeTrabajo _unidadDeTrabajo;
-    private readonly IValidator<ComandoCrearConsultorio> validator;
 
     public CasoDeUsoCrearConsultorio(
         IRepositorioConsultorios repositorio, 
-        IUnidadDeTrabajo unidadDeTrabajo,
-        IValidator<ComandoCrearConsultorio> validator
+        IUnidadDeTrabajo unidadDeTrabajo
     )
     {
         _repositorio = repositorio;
         _unidadDeTrabajo = unidadDeTrabajo;
-        this.validator = validator;
     }
 
 
@@ -31,13 +26,6 @@ public class CasoDeUsoCrearConsultorio : IRequestHandler<ComandoCrearConsultorio
         // Lógica para crear un consultorio - Orquestación del dominio
         // Por ejemplo, validar datos, guardar en base de datos, etc.
         // Simulación de creación y retorno de un ID único para el consultorio creado
-        var resultadoValidacion = await validator.ValidateAsync(comando);
-        if (!resultadoValidacion.IsValid)
-        {
-            //Excepcion personalizada
-            throw new ExcepcionDeValidacion(resultadoValidacion);
-        }
-
 
         var consultorio = new Consultorio(comando.Nombre);
         try
