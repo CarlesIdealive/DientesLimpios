b35328eea8df157a1b98badf58b98b3551575c77
Carles Labrana <carles.labrana@idealiveconsulting.com>
2026-03-26 13:02:57 +0100

    Elimina WeatherForecast y agrega endpoints de Consultorios
    Se eliminan el controlador, DTO y pruebas relacionadas con WeatherForecast, removiendo el ejemplo por defecto de ASP.NET. Se implementa ConsultoriosController con endpoints para crear y obtener consultorios usando Mediator. Se añade el DTO CrearConsultorioDTO con validaciones y un archivo de pruebas HTTP para los nuevos endpoints.

---
+++ DientesLimpios.API/Controllers/ConsultoriosController.cs
@@ -0,0 +1,37 @@
+﻿using DientesLimpios.API.DTOs.Consultorios;
+using DientesLimpios.Aplicacion.CasosDeUso.Consultorios.Comandos.CrearConsultorio;
+using DientesLimpios.Aplicacion.CasosDeUso.Consultorios.Consultas.ObtenerDetalleConsultorio;
+using DientesLimpios.Aplicacion.Utilidades.Mediador;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DientesLimpios.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ConsultoriosController : ControllerBase
+{
+    private readonly IMediator mediator;
+
+    public ConsultoriosController(IMediator mediator)
+    {
+        this.mediator = mediator;
+    }
+
+
+    [HttpPost]
+    public async Task<IActionResult> CrearConsultorio(CrearConsultorioDTO crearConsultorioDTO)
+    {
+        var comando = new ComandoCrearConsultorio { Nombre = crearConsultorioDTO.Nombre };
+        var resultado = await mediator.Send(comando);
+        return Ok(resultado);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<ConsultorioDetalleDTO>> ObtenerConsultorioPorId(Guid id)
+    {
+        // Aquí iría la lógica para obtener un consultorio por su ID
+        var resultado = await mediator.Send(new ConsultaObtenerDetalleConsultorio { Id = id }); 
+        return Ok(resultado);
+    }
+
+}
---
+++ DientesLimpios.API/DTOs/Consultorios/CrearConsultorioDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DientesLimpios.API.DTOs.Consultorios;
+
+public class CrearConsultorioDTO
+{
+    [Required]
+    [StringLength(100, MinimumLength = 3)]
+    public required string Nombre { get; set; } = string.Empty;
+
+}
