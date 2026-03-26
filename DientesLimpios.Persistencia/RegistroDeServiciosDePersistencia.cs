b71b6f82b271df76ea74863652bfe6366298494a
idealiveConsulting <idealiveConsulting@gmail.com>
2026-03-26 12:30:03 +0100

    Añade proyecto API, configuración y migraciones iniciales
    Se incorpora el nuevo proyecto DientesLimpios.API (ASP.NET Core Web API) con archivos de configuración, controlador y ejemplo. Se configura el pipeline de la API, se integran servicios de aplicación y persistencia, y se habilita OpenAPI. Se corrige el nombre de la cadena de conexión en persistencia. Se agregan migraciones iniciales para la tabla Consultorios y se actualiza la solución para incluir el nuevo proyecto.

--- DientesLimpios.Persistencia/RegistroDeServiciosDePersistencia.cs
+++ DientesLimpios.Persistencia/RegistroDeServiciosDePersistencia.cs
@@ -11,7 +11,7 @@ public static class RegistroDeServiciosDePersistencia
     public static IServiceCollection AgregarServiciosDePersistencia(this IServiceCollection servicios)
     {
         servicios.AddDbContext<DientesLimpiosDbContext>(options =>
-            options.UseSqlServer("name=DientesLimpiosconnectionString"));
+            options.UseSqlServer("name=DientesLimpiosConnectionString"));
 
         servicios.AddScoped<IRepositorioConsultorios, RepositorioConsultorios>();
         servicios.AddScoped<IUnidadDeTrabajo, UnidadesDeTrabajo.UnidadDeTrabajoEFCore>();
